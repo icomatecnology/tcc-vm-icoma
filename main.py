c61804fe0f31f0324ba84900b8c48214426c4830
victoricoma <victorsena3010@gmail.com>
2026-05-22 17:47:55 -0300

    Aula2205 -server index

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    nome = "Icoma.com"
+    nome = 'icoma.com.br'
     return render_template('index.html', site = nome)
 
 @app.route("/login")
